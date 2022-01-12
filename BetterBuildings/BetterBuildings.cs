a646a18cb2e7586f79e060fd413ffea34a285a27
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-01-12 14:43:07 -0600

    Start of project

---
+++ BetterBuildings/BetterBuildings.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+using System;
+
+namespace BetterBuildings
+{
+    internal class BetterBuildings : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
