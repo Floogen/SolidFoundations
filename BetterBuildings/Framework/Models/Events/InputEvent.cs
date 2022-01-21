fc636b3a57ba53d48229857a04da47d1e49acea9
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-01-20 21:03:27 -0600

    Added Dialogue, Input and Message events

---
+++ BetterBuildings/Framework/Models/Events/InputEvent.cs
@@ -0,0 +1,16 @@
+﻿using BetterBuildings.Framework.Models.ContentPack;
+using BetterBuildings.Framework.Models.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterBuildings.Framework.Models.Events
+{
+    public class InputEvent
+    {
+        public List<ItemModel> RequiredItems { get; set; } = new List<ItemModel>();
+        public MessageEvent BadInputMessage { get; set; } = new MessageEvent("Nothing interesting happens", MessageType.Quest);
+    }
+}
