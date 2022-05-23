014952123c1b2077cf905f80da166e109433769e
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-05-22 21:12:21 -0500

    Implemented ModifyInventory and ConditionalActions for SpecialActions

---
+++ SolidFoundations/Framework/Models/ContentPack/Actions/ModifyInventory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SolidFoundations.Framework.Models.ContentPack.Actions.SpecialAction;
+
+namespace SolidFoundations.Framework.Models.ContentPack.Actions
+{
+    public class ModifyInventory
+    {
+        public string ItemId { get; set; }
+        public int Quantity { get; set; } = 1;
+        public OperationName Operation { get; set; } = OperationName.Add;
+    }
+}
--- SolidFoundations/Framework/Models/ContentPack/Actions/SpecialAction.cs
+++ SolidFoundations/Framework/Models/ContentPack/Actions/SpecialAction.cs
@@ -33,19 +33,32 @@ namespace SolidFoundations.Framework.Models.ContentPack.Actions
             Remove
         }
 
+        public string Condition { get; set; }
+        public string[] ModDataFlags { get; set; }
+
         public DialogueAction Dialogue { get; set; }
         public MessageAction Message { get; set; }
         public WarpAction Warp { get; set; }
         public QuestionResponseAction DialogueWithChoices { get; set; }
-        public ModifyModDataAction ModifyFlag { get; set; }
+        public ModifyInventory ModifyInventory { get; set; }
+        public List<ModifyModDataAction> ModifyFlags { get; set; }
+        public List<SpecialAction> ConditionalActions { get; set; }
 
         public void Trigger(Farmer who, GenericBuilding building)
         {
+            if (ConditionalActions is not null)
+            {
+                var validAction = ConditionalActions.Where(d => building.ValidateConditions(d.Condition, d.ModDataFlags)).FirstOrDefault();
+                if (validAction is not null)
+                {
+                    validAction.Trigger(who, building);
+                }
+            }
             if (Dialogue is not null)
             {
                 Game1.activeClickableMenu = new DialogueBox(HandleSpecialTextTokens(Dialogue.Text));
             }
-            if (DialogueWithChoices is not null)
+            if (DialogueWithChoices is not null && building.ValidateConditions(this.Condition, this.ModDataFlags))
             {
                 List<Response> responses = new List<Response>();
                 foreach (var response in DialogueWithChoices.Responses.Where(r => r.SpecialAction is not null))
@@ -59,22 +72,36 @@ namespace SolidFoundations.Framework.Models.ContentPack.Actions
             {
                 Game1.addHUDMessage(new HUDMessage(Message.Text, (int)Message.Icon + 1));
             }
-            if (ModifyFlag is not null)
+            if (ModifyFlags is not null)
             {
-                // If FlagType is Temporary, then remove it from the building's modData before saving
-                var flagKey = String.Concat(ModDataKeys.FLAG_BASE, ".", ModifyFlag.Name.ToLower());
-                if (ModifyFlag.Operation is OperationName.Add)
+                foreach (var modifyFlag in ModifyFlags)
                 {
-                    building.modData[flagKey] = ModifyFlag.Type.ToString();
-                }
-                else if (ModifyFlag.Operation is OperationName.Remove && building.modData.ContainsKey(flagKey))
-                {
-                    building.modData.Remove(flagKey);
+                    // If FlagType is Temporary, then remove it from the building's modData before saving
+                    var flagKey = String.Concat(ModDataKeys.FLAG_BASE, ".", modifyFlag.Name.ToLower());
+                    if (modifyFlag.Operation is OperationName.Add)
+                    {
+                        building.modData[flagKey] = modifyFlag.Type.ToString();
+                    }
+                    else if (modifyFlag.Operation is OperationName.Remove && building.modData.ContainsKey(flagKey))
+                    {
+                        building.modData.Remove(flagKey);
+                    }
                 }
             }
             if (Warp is not null)
             {
                 Game1.warpFarmer(Warp.Map, Warp.DestinationTile.X, Warp.DestinationTile.Y, Warp.FacingDirection == -1 ? who.FacingDirection : Warp.FacingDirection);
+            }
+            if (ModifyInventory is not null)
+            {
+                if (ModifyInventory.Operation == OperationName.Add && Int32.TryParse(ModifyInventory.ItemId, out int id))
+                {
+                    Item itemToAdd = new StardewValley.Object(id, ModifyInventory.Quantity);
+                    if (itemToAdd is not null && who.couldInventoryAcceptThisItem(itemToAdd))
+                    {
+                        who.addItemToInventoryBool(itemToAdd);
+                    }
+                }
             }
         }
 
