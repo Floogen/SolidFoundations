b2ecffc82bc82694265e8d4bdfa261d87d35a689
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-05-29 17:38:11 -0500

    Removed unused Content Patcher API

--- SolidFoundations/Framework/Managers/ApiManager.cs
+++ SolidFoundations/Framework/Managers/ApiManager.cs
@@ -11,32 +11,12 @@ namespace SolidFoundations.Framework.Managers
     internal class ApiManager
     {
         private IMonitor _monitor;
-        private IContentPatcherApi _contentPatcherApi;
         private ISTFApi _shopTileFrameworkApi;
         private ISaveAnywhereApi _saveAnywhereApi;
 
         public ApiManager(IMonitor monitor)
         {
             _monitor = monitor;
-        }
-
-        internal bool HookIntoContentPatcher(IModHelper helper)
-        {
-            _contentPatcherApi = helper.ModRegistry.GetApi<IContentPatcherApi>("Pathoschild.ContentPatcher");
-
-            if (_contentPatcherApi is null)
-            {
-                _monitor.Log("Failed to hook into Pathoschild.ContentPatcher.", LogLevel.Error);
-                return false;
-            }
-
-            _monitor.Log("Successfully hooked into Pathoschild.ContentPatcher.", LogLevel.Debug);
-            return true;
-        }
-
-        public IContentPatcherApi GetContentPatcherApi()
-        {
-            return _contentPatcherApi;
         }
 
         internal bool HookIntoShopTileFramework(IModHelper helper)
