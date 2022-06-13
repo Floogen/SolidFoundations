dea04076ff6ec52194859badf927a2824acb88a7
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-06-13 16:17:03 -0500

    Changed the default value for RestrictedItem.MaxAllowed to -1

--- SolidFoundations/Framework/Models/ContentPack/InputFilter.cs
+++ SolidFoundations/Framework/Models/ContentPack/InputFilter.cs
@@ -24,7 +24,7 @@ namespace SolidFoundations.Framework.Models.ContentPack
         public class RestrictedItem
         {
             public List<string> RequiredTags { get; set; }
-            public int MaxAllowed { get; set; }
+            public int MaxAllowed { get; set; } = -1;
             public bool RejectWhileProcessing { get; set; }
             public string Condition { get; set; }
             public string[] ModDataFlags { get; set; }
