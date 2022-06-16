142ac072ea353d964be7575207b4cd89a2ce0882
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2022-06-16 15:54:12 -0500

    Removed unused property from SpecialAction.FadeAction

--- SolidFoundations/Framework/Models/ContentPack/Actions/FadeAction.cs
+++ SolidFoundations/Framework/Models/ContentPack/Actions/FadeAction.cs
@@ -9,7 +9,6 @@ namespace SolidFoundations.Framework.Models.ContentPack.Actions
 {
     public class FadeAction
     {
-        public int DurationInSeconds { get; set; }
         public float Speed { get; set; } = 0.02f;
         public SpecialAction ActionAfterFade { get; set; }
     }
